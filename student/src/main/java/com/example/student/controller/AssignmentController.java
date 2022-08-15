9ad4909d76779c269d7cfb8cd595b4e3505c1b56
thetaung zan <thetaungzan2022@gmail.com>
2022-08-15 20:24:14 +0630

    update

---
+++ student/src/main/java/com/example/student/controller/AssignmentController.java
@@ -0,0 +1,4 @@
+package com.example.student.controller;
+
+public class AssignmentController {
+}
--- student/src/main/java/com/example/student/controller/HomeController.java
+++ student/src/main/java/com/example/student/controller/HomeController.java
@@ -8,6 +8,6 @@ public class HomeController {
 
     @GetMapping({"/","/home"})
     public String home(){
-        return "dashboard";
+        return "STU01";
     }
 }
