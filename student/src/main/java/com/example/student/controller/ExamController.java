9b28a25630041a764539c82db25d4a73fbe5d61b
thetaung zan <thetaungzan2022@gmail.com>
2022-08-16 10:38:29 +0630

    update

--- student/src/main/java/com/example/student/controller/ExamController.java
+++ student/src/main/java/com/example/student/controller/ExamController.java
@@ -1,4 +1,14 @@
 package com.example.student.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
 public class ExamController {
+
+    @GetMapping("/exam")
+    public ModelAndView exam(){
+        return new ModelAndView("STU03","exams",null);
+    }
 }
