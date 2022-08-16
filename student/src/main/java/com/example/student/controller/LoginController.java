1b1dd344580ee3ee7e5e8ce70500d4cb92191ce4
thetaung zan <thetaungzan2022@gmail.com>
2022-08-16 09:58:00 +0630

    update

--- student/src/main/java/com/example/student/controller/LoginController.java
+++ student/src/main/java/com/example/student/controller/LoginController.java
@@ -1,4 +1,13 @@
 package com.example.student.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class LoginController {
+
+    @GetMapping("/login")
+    public String login(){
+        return "LGN01";
+    }
 }
